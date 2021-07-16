c89063d9920eff6cc8512ce682764c79e4ff830e
Mahesh Patil <maheshp@cdac.in>
2021-07-16 15:17:58 +0530

    Optimized version

--- thread/threaddemo.c
+++ thread/threaddemo.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void *uart1_init(void *data)
+void *uart_init(void *data)
 {
-    printf("In Thread UART1: %d\n", *(int *)data);
-}
-
-void *uart2_init(void *data)
-{
-    printf("In Thread UART2: %d\n", *(int *)data);
+    printf("In Thread UART: %d\n", *(int *)data);
 }
 
 int main(int argc, char const *argv[])
@@ -19,9 +14,9 @@ int main(int argc, char const *argv[])
 
     printf("Before thread creation\n");
     baudrate1 = 9600;
-    pthread_create(&uart1_tid, NULL, uart1_init, &baudrate1);
+    pthread_create(&uart1_tid, NULL, uart_init, &baudrate1);
     baudrate2 = 115200;
-    pthread_create(&uart2_tid, NULL, uart2_init, &baudrate2);
+    pthread_create(&uart2_tid, NULL, uart_init, &baudrate2);
     printf("After thread creation\n");
     pthread_join(uart1_tid, NULL);
     pthread_join(uart2_tid, NULL);
